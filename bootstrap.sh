cca93dfc806d4761575eafd3670dfe3a60f752b4
ldraney <draneylucas@gmail.com>
2022-10-13 12:59:54 -0600

    quick

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,14 +5,14 @@ set -e    #I need to restart computer after this command for vm shared folder to
 #echo vmhgfs-fuse /mnt/hgfs  fuse defaults,allow_other   0   0 | sudo tee -a /etc/fstab
 #sudo mount -a
 mkdir -p $HOME/.ssh
-cp -r /mnt/c/Users/draney/Desktop/VMShare/ssh/* $HOME/.ssh/
+sudo cp -r /mnt/c/Users/drane/Desktop/VMShare/ssh/* $HOME/.ssh/
 
 chmod 400 $HOME/.ssh/*
 chown -R ldraney:ldraney $HOME/.ssh
 
 #rest of the install
-apt-get update
-apt-get upgrade -y
+sudo apt-get update
+sudo apt-get upgrade -y
 sudo apt-get install -y git ansible
 
 eval "$(ssh-agent -s)"
@@ -27,5 +27,6 @@ git clone git@github.com:ldraney/sensitive.git
 
 #use this for developing my ansible script
 #ansible-pull -U ssh://git@github.com/ldraney/ansible_desktop_setup.git --key-file $HOME/.ssh/id_ed25519 --accept-host-key --ask-become-pass
+cd ansible*
 sudo ansible-playbook local.yml
 sudo reboot now
