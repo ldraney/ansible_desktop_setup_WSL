d8d95ae9988cf2ffe23fe5dd6ed688df26ac8872
ldraney <draneylucas@gmail.com>
2022-10-24 17:06:45 -0600

    bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,22 +1,18 @@
 #!/bin/bash
-#vmware shared folder
-set -e    #I need to restart computer after this command for vm shared folder to be setup right, only if
-# this gives an error
-#echo vmhgfs-fuse /mnt/hgfs  fuse defaults,allow_other   0   0 | sudo tee -a /etc/fstab
-#sudo mount -a
+
+set -e
 mkdir -p $HOME/.ssh
 sudo cp -r /mnt/c/Users/drane/Desktop/VMShare/ssh/* $HOME/.ssh/
-
 sudo chmod 400 $HOME/.ssh/*
 sudo chown -R ldraney:ldraney $HOME/.ssh
+eval `ssh-agent`
+ssh-add $HOME/.ssh/id_ed*
 
-#rest of the install
-sudo apt-get update
-#sudo apt-get upgrade -y
-sudo apt-get install -y git ansible
-
-eval "$(ssh-agent -s)"
-ssh-add $HOME/.ssh/id_ed25519
+nix-env -iA nixpkgs.git
+nix-env -iA nixpkgs.ansible
+nix-env -iA nixpkgs.zsh
+nix-env -iA nixpkgs.tmux
+nix-env -iA nixpkgs.wget
 
 mkdir $HOME/github
 cd $HOME/github
@@ -25,8 +21,6 @@ git clone git@github.com:ldraney/ansible_desktop_setup_WSL.git
 git clone git@github.com:ldraney/dotfilesWSL.git
 git clone git@github.com:ldraney/sensitive.git
 
-#use this for developing my ansible script
-#ansible-pull -U ssh://git@github.com/ldraney/ansible_desktop_setup.git --key-file $HOME/.ssh/id_ed25519 --accept-host-key --ask-become-pass
 cd ansible*
-sudo ansible-playbook local.yml
-sudo reboot now
+ansible-playbook local.yml
+#sudo reboot now
