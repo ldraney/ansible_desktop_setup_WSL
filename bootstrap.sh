de605202e8bbe0d3874b2fc763bb2807835a75de
Lucas Draney <107153866+ldraney@users.noreply.github.com>
2022-10-24 17:26:42 -0600

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,7 @@
 
 #set -e
 sh <(curl -L https://nixos.org/nix/install) --no-daemon
+. $HOME/.nix-profile/etc/profile.d/nix.sh
 mkdir -p $HOME/.ssh
 sudo cp -r /mnt/c/Users/drane/Desktop/VMShare/ssh/* $HOME/.ssh/
 sudo chmod 400 $HOME/.ssh/*
