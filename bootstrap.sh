bce6949396737d999acf0fe5591d69ffef8d4b25
ldraney <draneylucas@gmail.com>
2022-10-26 13:09:20 -0600

    tmux terminate left and right panes

--- bootstrap.sh
+++ bootstrap.sh
@@ -26,10 +26,10 @@ nix-env -iA nixpkgs.htop
 nix-env -iA nixpkgs.mlocate
 nix-env -iA nixpkgs.wget
 nix-env -iA nixpkgs.feh
+nix-env -iA nixpkgs.google-cloud-sdk
 #Docker is installed on WSL2 by docker desktop.  With autocomplete as well!
 #nix-env -iA nixpkgs.docker
 #nix-env -iA nixpkgs.docker-compose
-nix-env -iA nixpkgs.google-cloud-sdk
 
 mkdir $HOME/github
 cd $HOME/github
