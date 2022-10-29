6fb766c0efa8d76cf8651825f42b828705231693
ldraney <draneylucas@gmail.com>
2022-10-28 20:58:58 -0600

    nvim installed again

--- bootstrap.sh
+++ bootstrap.sh
@@ -24,6 +24,7 @@ nix-env -iA nixpkgs.ansible
 nix-env -iA nixpkgs.zsh
 nix-env -iA nixpkgs.tmux
 nix-env -iA nixpkgs.wget
+nix-env -iA nixpkgs.neovim
 nix-env -iA nixpkgs.terraform
 nix-env -iA nixpkgs.awscli2
 nix-env -iA nixpkgs.kubectl
