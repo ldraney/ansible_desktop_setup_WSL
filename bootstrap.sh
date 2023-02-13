c0707f2f654221f4420a3239ee5c441df1ade9c3
ldraney <draneylucas@gmail.com>
2023-02-13 09:39:05 -0700

    updated bootstrap to not have pyenv for now

--- bootstrap.sh
+++ bootstrap.sh
@@ -47,12 +47,12 @@ sudo apt install -y neovim
 sudo update-alternatives --install /usr/bin/vim vim /usr/bin/nvim 60
 
 #install pyenv
-curl https://pyenv.run | bash
-sudo apt-get update; sudo apt-get install -y make build-essential libssl-dev zlib1g-dev \
-	libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
-	libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
-#export PATH=$PATH:/home/ldraney/.pyenv/bin/pyenv
-pyenv install 3.11.0
+#curl https://pyenv.run | bash
+#sudo apt-get update; sudo apt-get install -y make build-essential libssl-dev zlib1g-dev \
+	#libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
+	#libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+##export PATH=$PATH:/home/ldraney/.pyenv/bin/pyenv
+#pyenv install 3.11.0
 
 #github extensions
 gh extension install geoffreywiseman/gh-actuse
