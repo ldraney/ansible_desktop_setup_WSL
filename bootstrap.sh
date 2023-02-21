ac78fd9581a924aeae342f2f909a3dd3bd6b2553
ldraney <draneylucas@gmail.com>
2023-02-21 06:19:37 -0700

    added nodejs to bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -54,7 +54,6 @@ ansible-playbook local.yml
 
 sudo usermod -aG docker ${USER}
 
-
 #install nodejs
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
 source ~/.nvm/nvm.sh
