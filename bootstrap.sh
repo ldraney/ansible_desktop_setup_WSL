c7e233da47922f046d15d9b6b2a1d104a02f21ae
ldraney <draneylucas@gmail.com>
2023-02-22 06:21:05 -0700

    latest

--- bootstrap.sh
+++ bootstrap.sh
@@ -65,7 +65,7 @@ curl https://pyenv.run | bash
 sudo apt-get update; sudo apt-get install -y make build-essential libssl-dev zlib1g-dev \
 	libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
 	libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
-#export PATH=$PATH:/home/ldraney/.pyenv/bin/pyenv
+export PATH=$PATH:/home/ldraney/.pyenv/bin/pyenv
 pyenv install 3.11.0
 
 #install nodejs
