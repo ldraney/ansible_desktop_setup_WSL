690fc95528964d07e0bbb75541161b85f86db965
ldraney <draneylucas@gmail.com>
2023-12-17 14:57:46 -0700

    been starting on memgpt

--- bootstrap.sh
+++ bootstrap.sh
@@ -86,5 +86,29 @@ nvm install --lts
 
 #install terraform
 git clone --depth=1 https://github.com/tfutils/tfenv.git ~/.tfenv
-tfenv install 1.1.3
-tfenv use 1.1.3
+# latest terraform
+#zsh
+#tfenv install 1.6.5
+#tfenv use 1.6.5
+
+# LUAROCKS
+# install LuaRocks for Neovim
+sudo apt-get install build-essential libreadline-dev # probably unecessary
+
+# install lua
+cd
+curl -R -O http://www.lua.org/ftp/lua-4.3.5.tar.gz
+tar -zxf lua-5.3.5.tar.gz
+cd lua-5.3.5
+make linux test
+sudo make install
+
+wget http://luarocks.github.io/luarocks/releases/luarocks-3.9.2.tar.gz
+
+tar zxpf luarocks-3.9.2.tar.gz
+cd luarocks-3.9.2
+./configure --with-lua-include=/usr/local/include
+make
+sudo make install
+sudo luarocks install busted
+
