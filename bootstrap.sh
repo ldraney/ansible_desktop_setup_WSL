6a0eb5581720314893bb58b40dfee39967c2c1a9
Lucas Draney <107153866+ldraney@users.noreply.github.com>
2024-06-08 17:01:26 -0600

    successful clone of iss and personal repos

--- bootstrap.sh
+++ bootstrap.sh
@@ -22,52 +22,27 @@ sudo update-alternatives --install /usr/bin/vim vim /usr/bin/nvim 60
 ## assumes you have manually added to the bashrc GITHUB_TOKEN from the sensitive repo
 mkdir -p $HOME/personal
 cd $HOME/personal
-repos=("sensitive" "dotfilesWSL")
+repos=("sensitive" "dotfilesWSL" "ansible_desktop_setup_WSL" "kickstart.nvim")
 for repo in "${repos[@]}"; do
-  git clone https://$GITHUB_TOKEN@github.com/ldraney/$repo.git
+  git clone https://$GITHUB_TOKEN@github.com/ldraney/$repo.git &
 done
+wait
 
-# Set up ansible in personal repos directory
-cd $HOME/personal
-git clone --bare git@github.com:ldraney/ansible_desktop_setup_WSL.git ansible_desktop_setup_WSL
-cd ansible*
-git worktree add master
-git worktree add $ANSIBLE_BRANCH
+# iss repos
+mkdir -p $HOME/iss
+cd $HOME/iss
+repos=("iss-setter-afi-frontend" "iss-setter-afi-backend" "iss-setter-afi-backend.wiki")
+for repo in "${repos[@]}"; do
+  git clone https://$GITHUB_TOKEN@github.com/dialectic-devops/$repo.git &
+done
+wait
 
-# Set up kickstart.nvim in personal repos directory
-# This is important because symlinks for nvim setup will soon go here instead of the dotfiles repo
-cd $HOME/personal
-git clone --bare git@github.com:ldraney/kickstart.nvim.git kickstart.nvim
-cd kickstart.nvim
-git worktree add master
-git worktree add $ANSIBLE_BRANCH
-
-# # iss repos
-# mkdir -p $HOME/iss
-# cd $HOME/iss
-# git clone git@github.com:ldraney/iss-setter-docs.git
-# git clone git@github.com:ldraney/iss-setter-api.git
-# git clone git@github.com:ldraney/iss-setter-app.git
-# git clone git@github.com:dialectic-devops/iss-setter-infra.git
-# git clone git@github.com:ldraney/iss-helps.git
-
-# #install cheatsheet
+#install cheatsheet
 cd /tmp \
   && wget https://github.com/cheat/cheat/releases/download/4.4.0/cheat-linux-amd64.gz \
   && gunzip cheat-linux-amd64.gz \
   && chmod +x cheat-linux-amd64 \
   && sudo mv cheat-linux-amd64 /usr/local/bin/cheat
-
-# #Clone oddball repos
-cd $HOME
-mkdir oddball
-cd oddball
-# git clone --bare git@github.com:department-of-veterans-affairs/vanotify-team.git vanotify-team
-# git clone --bare git@github.com:department-of-veterans-affairs/vanotify-infra.git vanotify-infra
-# git clone --bare git@github.com:department-of-veterans-affairs/notification-api.git notification-api
-# git clone --bare git@github.com:department-of-veterans-affairs/notification-api-qa.git notification-api-qa
-# git clone --bare git@github.com:department-of-veterans-affairs/notification-kafka.git notification-kafka
-# git clone --bare git@github.com:department-of-veterans-affairs/notification-utils.git notification-utils
 
 # Use Ansible for setting up:
 # - symlinks
