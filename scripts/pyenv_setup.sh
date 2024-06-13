f1a9999ba03bc15dbf6c3d31ea2f156dab6a9313
ldraney <draneylucas@gmail.com>
2024-06-13 12:01:42 -0600

    pyenv script fix

--- scripts/pyenv_setup.sh
+++
@@ -1,6 +0,0 @@
-curl https://pyenv.run | bash
-sudo apt-get update
-sudo apt-get install -y make build-essential libssl-dev zlib1g-dev \
-	libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
-	libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
-/home/ldraney/.pyenv/bin/pyenv install 3.8.10
