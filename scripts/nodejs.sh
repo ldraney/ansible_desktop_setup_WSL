386447812f01a92b1fcde40127e288ae10908361
ldraney <draneylucas@gmail.com>
2024-06-17 18:21:09 -0600

    install node script

--- scripts/nodejs.sh
+++ scripts/nodejs.sh
@@ -1,4 +1,4 @@
 # #install nodejs 
-curl -o- https://ruw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
 source ~/.nvm/nvm.sh
 nvm install --lts
