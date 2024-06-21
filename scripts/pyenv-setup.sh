86193a2db4c1cf374e018add7956749a3850db89
Lucas Draney <107153866+ldraney@users.noreply.github.com>
2024-06-21 11:36:33 -0600

    Update pyenv-setup.sh

--- scripts/pyenv-setup.sh
+++ scripts/pyenv-setup.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 echo "STARTING PYENV SETUP SCRIPT"
 
+# Pyenv stuff
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
+
 sudo apt-get update; sudo apt-get install -y make build-essential libssl-dev zlib1g-dev \
 	libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
 	libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
